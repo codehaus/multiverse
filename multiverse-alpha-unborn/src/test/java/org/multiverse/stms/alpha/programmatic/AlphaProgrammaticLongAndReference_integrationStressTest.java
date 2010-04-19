1c7bf68357f29718308d522a9eaf0535517cf02d
alarmnummer <alarmnummer@xps1530.(none)>
2010-04-19 17:27:55 +0200

    gradle stuff and more tests

---
+++ multiverse-alpha-unborn/src/test/java/org/multiverse/stms/alpha/programmatic/AlphaProgrammaticLongAndReference_integrationStressTest.java
@@ -0,0 +1,180 @@
+package org.multiverse.stms.alpha.programmatic;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.multiverse.TestThread;
+import org.multiverse.annotations.TransactionalMethod;
+import org.multiverse.annotations.TransactionalObject;
+import org.multiverse.api.programmatic.ProgrammaticLong;
+import org.multiverse.api.programmatic.ProgrammaticReference;
+import org.multiverse.api.programmatic.ProgrammaticReferenceFactory;
+
+import static org.multiverse.TestUtils.joinAll;
+import static org.multiverse.TestUtils.startAll;
+import static org.multiverse.api.GlobalStmInstance.getGlobalStmInstance;
+import static org.multiverse.api.StmUtils.retry;
+import static org.multiverse.api.ThreadLocalTransaction.clearThreadLocalTransaction;
+
+/**
+ * An integration tests that builds a queue (based on 2 stacks) based on the programmatic
+ * reference api. It uses an AlphaProgrammaticLong and AlphaProgrammaticRef.
+ *
+ * @author Peter Veentjer
+ */
+public class AlphaProgrammaticLongAndReference_integrationStressTest {
+
+    private int threadCount = 10;
+    private int transactionCount = 1000 * 1000;
+    private int queueCapacity = 1000;
+
+    private final static ProgrammaticReferenceFactory refFactory = getGlobalStmInstance()
+            .getProgrammaticReferenceFactoryBuilder()
+            .build();
+
+    private Queue<String> queue;
+
+    @Before
+    public void setUp() {
+        clearThreadLocalTransaction();
+        queue = new Queue<String>();
+    }
+
+    @Before
+    public void tearDown() {
+        clearThreadLocalTransaction();
+    }
+
+    @Test
+    public void test() {
+        ProducerThread[] producers = new ProducerThread[threadCount];
+        ConsumerThread[] consumers = new ConsumerThread[threadCount];
+        for (int k = 0; k < threadCount; k++) {
+            producers[k] = new ProducerThread(k);
+            consumers[k] = new ConsumerThread(k);
+        }
+
+        startAll(producers);
+        startAll(consumers);
+        joinAll(producers);
+        joinAll(consumers);
+    }
+
+    public class ProducerThread extends TestThread {
+        public ProducerThread(int id) {
+            super("ProducerThread-" + id);
+        }
+
+        @Override
+        public void doRun() throws Exception {
+            for (int k = 0; k < transactionCount; k++) {
+                produce();
+
+                if (k % 100000 == 0) {
+                    System.out.printf("%s is at %s\n", getName(), k);
+                }
+            }
+        }
+
+        @TransactionalMethod
+        private void produce() {
+            queue.push("foo");
+        }
+    }
+
+    public class ConsumerThread extends TestThread {
+        public ConsumerThread(int id) {
+            super("ConsumerThread-" + id);
+        }
+
+        @Override
+        public void doRun() throws Exception {
+            for (int k = 0; k < transactionCount; k++) {
+                produce();
+
+                if (k % 100000 == 0) {
+                    System.out.printf("%s is at %s\n", getName(), k);
+                }
+            }
+        }
+
+        @TransactionalMethod
+        private void produce() {
+            queue.pop();
+        }
+    }
+
+    @TransactionalObject
+    public class Queue<E> {
+        private final Stack<E> queuedStack = new Stack<E>();
+        private final Stack<E> popableStack = new Stack<E>();
+        private final ProgrammaticLong size = refFactory.atomicCreateLong(0);
+
+        public void push(E item) {
+            if (size() > queueCapacity) {
+                retry();
+            }
+
+            size.inc(1);
+            queuedStack.push(item);
+        }
+
+        public E pop() {
+            if (queuedStack.head.isNull() && popableStack.head.isNull()){
+                retry();
+            }
+
+            size.inc(-1);
+            if (!popableStack.isEmpty()) {
+                return popableStack.pop();
+            }
+
+            while (!queuedStack.isEmpty()) {
+                popableStack.push(queuedStack.pop());
+            }
+
+            return popableStack.pop();
+        }
+
+        public int size() {
+            return (int)size.get();
+        }
+    }
+
+    @TransactionalObject
+    public class Stack<E> {
+
+        final ProgrammaticReference<Node<E>> head = refFactory.atomicCreateReference(null);
+
+        public boolean isEmpty() {
+            return head.isNull();
+        }
+
+        void push(E item) {
+            if (item == null) {
+                throw new NullPointerException();
+            }
+
+            head.set(new Node<E>(item, head.get()));
+        }
+
+        E pop() {
+            if (head.isNull()) {
+                retry();
+            }
+
+            Node<E> h = head.get();
+            head.set(h.next);
+            return h.value;
+        }
+
+        class Node<E> {
+            final E value;
+            final Node<E> next;
+
+            Node(E value, Node<E> next) {
+                this.value = value;
+                this.next = next;
+            }
+        }
+    }
+}
---
+++ multiverse-alpha-unborn/src/test/java/org/multiverse/stms/alpha/programmatic/AlphaProgrammaticLong_atomicBehaviorTest.java
@@ -0,0 +1,7 @@
+package org.multiverse.stms.alpha.programmatic;
+
+/**
+ * @author Peter Veentjer
+ */
+public class AlphaProgrammaticLong_atomicBehaviorTest {
+}
