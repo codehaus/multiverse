4763fafd3ea25b70bb1ac7f6165b4c428d6ef282
alarmnummer <alarmnummer@alarmnummer-desktop.(none)>
2010-04-23 22:59:11 +0200

    - added a small performance test tool that calculates the theoretical maximum performance of the stm on a system

---
+++ multiverse-performance-tool/src/main/java/org/multiverse/performancetool/PerformanceTool.java
@@ -0,0 +1,150 @@
+package org.multiverse.performancetool;
+
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * @author Peter Veentjer
+ */
+public class PerformanceTool {
+
+    public static void main(String[] args) throws InterruptedException {
+        PerformanceToolArguments cli = createCli(args);
+        PerformanceTool multiverseCompiler = new PerformanceTool();
+        multiverseCompiler.run(cli);
+    }
+
+    private void run(PerformanceToolArguments arguments) throws InterruptedException {
+        System.out.println("Multiverse: Starting Performance Tool");
+
+        printSettings(arguments);
+        printWarning();
+        long durationNs = executeBenchmark(arguments);
+        printResults(arguments, durationNs);
+    }
+
+    private void printResults(PerformanceToolArguments cli, long durationNs) {
+        double transactionsPerSecondPerThread = (1.0d * cli.transactionCount * TimeUnit.SECONDS.toNanos(1)) / durationNs;
+        double transactionsPerSecond = transactionsPerSecondPerThread * cli.threadCount;
+
+        System.out.println("[finished]----------------------------------------------------------");
+        System.out.printf("Duration: %s seconds\n", TimeUnit.NANOSECONDS.toSeconds(durationNs));
+        System.out.printf("Duration: %s nanoseconds\n", durationNs);
+        System.out.printf("Transactions/second: %s \n", transactionsPerSecond);
+        System.out.printf("Transactions/second: %s per core\n", transactionsPerSecondPerThread);
+    }
+
+    private long executeBenchmark(PerformanceToolArguments arguments) throws InterruptedException {
+
+        AtomicLong clock = new AtomicLong();
+
+        Barrier startBarrier = new Barrier();
+        Thread[] threads = new Thread[arguments.threadCount];
+        for (int k = 0; k < arguments.threadCount; k++) {
+            threads[k] = new StressThread(k, arguments.transactionCount, clock, arguments.strict, startBarrier);
+            threads[k].start();
+        }
+
+
+        System.out.println("[starting]----------------------------------------------------------");
+
+        long startNs = System.nanoTime();
+        startBarrier.open();
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        return System.nanoTime() - startNs;
+    }
+
+    private void printSettings(PerformanceToolArguments cli) {
+        System.out.println("Transaction count per thread = " + cli.transactionCount);
+        System.out.println("Threadcount = " + cli.threadCount);
+        System.out.println("Strict = " + cli.strict);
+    }
+
+    private void printWarning() {
+        System.out.println("[info]---------------------------------------------------------");
+        System.out.println("Watch out with hyperthreading since the number of virtual cores");
+        System.out.println("increase but the actual number of cores remains the same. In");
+        System.out.println("this benchmark hyperthreading could cause a performance");
+        System.out.println("slowdown, because multiple threads will compete for the same");
+        System.out.println("non virtual core. Use the -t argument for configuring the number");
+        System.out.println("of threads.");
+    }
+
+    static class Barrier {
+        private boolean open = false;
+
+        public synchronized void open() {
+            open = true;
+            notifyAll();
+        }
+
+        public synchronized void awaitOpen() {
+            while (!open) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    private class StressThread extends Thread {
+        private final long transactionCount;
+        private final AtomicLong clock;
+        private final boolean strict;
+        private Barrier barrier;
+
+        public StressThread(int id, long transactionCount, AtomicLong clock, boolean strict, Barrier barrier) {
+            super("StressThread-" + id);
+            this.transactionCount = transactionCount;
+            this.clock = clock;
+            this.strict = strict;
+            this.barrier = barrier;
+        }
+
+        public void run() {
+            barrier.awaitOpen();
+
+            for (long k = 0; k < transactionCount; k++) {
+                if (strict) {
+                    clock.incrementAndGet();
+                } else {
+                    long value = clock.get();
+                    clock.compareAndSet(value, value + 1);
+                }
+
+                if (k > 0 && k % 10000000 == 0) {
+                    System.out.printf("%s is at %s\n", getName(), k);
+                }
+            }
+        }
+    }
+
+    private static PerformanceToolArguments createCli(String[] args) {
+        CmdLineParser parser = null;
+        try {
+            PerformanceToolArguments cli = new PerformanceToolArguments();
+            parser = new CmdLineParser(cli);
+            parser.parseArgument(args);
+            return cli;
+        } catch (CmdLineException e) {
+            e.printStackTrace();
+            System.err.println(e.getMessage());
+            System.err.println("java -jar myprogram.jar [options...]");
+            if (parser != null) {
+                parser.printUsage(System.out);
+            }
+            System.exit(-1);
+            return null;
+        }
+    }
+
+}
---
+++ multiverse-performance-tool/src/main/java/org/multiverse/performancetool/PerformanceToolArguments.java
@@ -0,0 +1,23 @@
+package org.multiverse.performancetool;
+
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.Option;
+
+/**
+ * @author Peter Veentjer
+ */
+public class PerformanceToolArguments {
+
+    @Option(name = "-t",
+            required = false,
+            usage = "The number of threads (default is number of available processors")
+    public int threadCount = Runtime.getRuntime().availableProcessors();
+
+    @Argument(required = false, index = 0, metaVar = "TRANSACTION_COUNT",
+            usage = "The number of transactions per thread")
+    public long transactionCount = 1000 * 1000 * 100;
+
+    @Option(name = "-s", usage = "If the tick should be strict (multiverse can deal with relaxed ticks)", required = false)
+    public boolean strict = false;
+
+}
