1c74688e0ed897ef5f1f17c18c18889b6258ab95
Sai Venkat <s.sai.venkat@gmail.com>
2010-05-07 04:01:31 +0800

    Global instance tests

--- multiverse-core/src/test/java/org/multiverse/api/GlobalStmInstanceTest.java
+++ multiverse-core/src/test/java/org/multiverse/api/GlobalStmInstanceTest.java
@@ -1,12 +1,30 @@
 package org.multiverse.api;
 
-import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.Test;
+import org.multiverse.api.programmatic.ProgrammaticReferenceFactoryBuilder;
+
+import static org.junit.Assert.*;
+import static org.multiverse.api.GlobalStmInstance.getGlobalStmInstance;
 /**
- * @author Peter Veentjer
+ * @author Sai Venkat
  */
 public class GlobalStmInstanceTest {
-
+    private Stm stm;
+    private Stm stm1;
+    private Stm stm2;
+    
     @Test
-    public void test(){}
+    public void GiveMePreConfiguredSTMInstance(){
+        stm = getGlobalStmInstance();
+        assertThat(stm.getTransactionFactoryBuilder(), is(instanceOf(TransactionFactoryBuilder.class)));
+        assertThat(stm.getProgrammaticReferenceFactoryBuilder(), is(instanceOf(ProgrammaticReferenceFactoryBuilder.class)));
+    }
+    @Test
+    public void GiveMeSingleInstanceOfSTM() {
+        stm1 = getGlobalStmInstance();
+        stm2 = getGlobalStmInstance();
+        assertThat(stm1, is(sameInstance(stm2)));
+    }
 }
