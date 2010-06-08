457a7255bcab9afb5e885f34950c3217160d66a0
peter <alarmnummer@gmail.com>
2010-06-08 21:51:48 +0200

    - added missing empty tests

--- multiverse-alpha-unborn/src/test/java/org/multiverse/stms/alpha/instrumentation/integrationtest/WhileLoopTest.java
+++ multiverse-alpha-unborn/src/test/java/org/multiverse/stms/alpha/instrumentation/integrationtest/WhileLoopTest.java
@@ -1,7 +1,14 @@
 package org.multiverse.stms.alpha.instrumentation.integrationtest;
 
+import org.junit.Ignore;
+import org.junit.Test;
+
 /**
  * @author Peter Veentjer
  */
 public class WhileLoopTest {
+
+    @Test
+    @Ignore
+    public void test(){}
 }
