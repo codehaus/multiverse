aaa3f400e756fb24c58916c9cf567ae7e577da78
alarmnummer <alarmnummer@gmail.com>
2010-08-28 10:29:59 +0200

    - moved the benchmarks scripts to seperate directory

---
+++ benchmarks/run-update.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+java -version
+java -XX:+UseParallelGC -XX:+UseParallelOldGC -server -da \
+    -cp ../out/production/snzi:../lib/junit-4.8.1.jar org.multiverse.benchmarks.UncontendedUpdateScalabilityTest  250000000
