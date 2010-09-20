b20286f9bffb43330f66c0631fba44461bc6ccbf
alarmnummer <alarmnummer@gmail.com>
2010-09-21 01:53:05 +0200

    - added the aba test

---
+++ src/test/java/org/multiverse/stms/beta/integrationtest/isolation/AbaTest.java
@@ -0,0 +1,41 @@
+package org.multiverse.stms.beta.integrationtest.isolation;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.multiverse.api.exceptions.ReadWriteConflict;
+import org.multiverse.stms.beta.BetaStm;
+import org.multiverse.stms.beta.transactionalobjects.BetaLongRef;
+import org.multiverse.stms.beta.transactions.BetaTransaction;
+
+import static org.junit.Assert.fail;
+import static org.multiverse.api.ThreadLocalTransaction.clearThreadLocalTransaction;
+import static org.multiverse.stms.beta.BetaStmUtils.newLongRef;
+
+public class AbaTest {
+    private BetaStm stm;
+
+    @Before
+    public void setUp() {
+        stm = new BetaStm();
+        clearThreadLocalTransaction();
+    }
+
+    @Test
+    public void test() {
+        BetaLongRef ref = newLongRef(stm);
+
+        BetaTransaction tx = stm.startDefaultTransaction();
+        ref.get(tx);
+
+        ref.atomicIncrementAndGet(1);
+        ref.atomicIncrementAndGet(-1);
+
+        ref.incrementAndGet(tx, 1);
+
+        try {
+            tx.commit();
+            fail();
+        } catch (ReadWriteConflict expected) {
+        }
+    }
+}
