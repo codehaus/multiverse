80dcd6d9525ef13edcaa0fd449d90275d5c1dcf7
alarmnummer <alarmnummer@gmail.com>
2010-10-09 16:47:02 +0200

    - more testing

---
+++ src/test/java/org/multiverse/stms/beta/transactionalobjects/BetaLongRef_atomicIsEnsuredTest.java
@@ -0,0 +1,72 @@
+package org.multiverse.stms.beta.transactionalobjects;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.multiverse.stms.beta.BetaStm;
+import org.multiverse.stms.beta.transactions.BetaTransaction;
+
+import static org.junit.Assert.*;
+import static org.multiverse.stms.beta.BetaStmTestUtils.assertVersionAndValue;
+import static org.multiverse.stms.beta.BetaStmTestUtils.newLongRef;
+import static org.multiverse.stms.beta.orec.OrecTestUtils.*;
+
+public class BetaLongRef_atomicIsEnsuredTest {
+
+     private BetaStm stm;
+
+    @Before
+    public void setUp(){
+        stm = new BetaStm();
+    }
+
+    @Test
+    public void whenIsFree(){
+        long initialValue = 10;
+        BetaLongRef ref = newLongRef(stm, initialValue);
+        long initialVersion = ref.getVersion();
+
+        boolean result = ref.atomicIsEnsured();
+
+        assertFalse(result);
+        assertVersionAndValue(ref, initialVersion, initialValue);
+        assertNull(ref.___getLockOwner());
+        assertHasNoCommitLock(ref);
+        assertHasNoUpdateLock(ref);
+    }
+
+    @Test
+    public void whenIsEnsured(){
+        long initialValue = 10;
+        BetaLongRef ref = newLongRef(stm, initialValue);
+        long initialVersion = ref.getVersion();
+
+        BetaTransaction tx = stm.startDefaultTransaction();
+        ref.ensure(tx);
+
+        boolean result = ref.atomicIsEnsured();
+
+        assertTrue(result);
+        assertVersionAndValue(ref, initialVersion, initialValue);
+        assertSame(tx, ref.___getLockOwner());
+        assertHasNoCommitLock(ref);
+        assertHasUpdateLock(ref);
+    }
+
+    @Test
+    public void whenIsPrivatized(){
+        long initialValue = 10;
+        BetaLongRef ref = newLongRef(stm, initialValue);
+        long initialVersion = ref.getVersion();
+
+        BetaTransaction tx = stm.startDefaultTransaction();
+        ref.privatize(tx);
+
+        boolean result = ref.atomicIsEnsured();
+
+        assertFalse(result);
+        assertVersionAndValue(ref, initialVersion, initialValue);
+        assertSame(tx, ref.___getLockOwner());
+        assertHasCommitLock(ref);
+        assertHasNoUpdateLock(ref);
+    }
+}
