4bd20722fc8e7417a059737b46005d461ddfc5f1
alarmnummer <alarmnummer@gmail.com>
2010-10-13 03:24:29 +0200

    - more testing on blocking

--- src/test/java/org/multiverse/stms/beta/integrationtest/blocking/NoBlockingTest.java
+++ src/test/java/org/multiverse/stms/beta/integrationtest/blocking/NoBlockingTest.java
@@ -11,21 +11,35 @@ import org.multiverse.api.references.LongRef;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import static org.multiverse.api.GlobalStmInstance.getGlobalStmInstance;
-import static org.multiverse.api.StmUtils.newLongRef;
-import static org.multiverse.api.StmUtils.retry;
+import static org.multiverse.api.StmUtils.*;
 import static org.multiverse.api.ThreadLocalTransaction.clearThreadLocalTransaction;
 
-public class NoBlockingAllowedTest {
+public class NoBlockingTest {
     @Before
     public void setUp() {
         clearThreadLocalTransaction();
     }
 
     @Test
+    public void whenNothingRead_thenNoRetryPossibleException(){
+        try {
+            execute(new AtomicVoidClosure() {
+                @Override
+                public void execute(Transaction tx) throws Exception {
+                    retry();
+                }
+            });
+            fail();
+        } catch (NoRetryPossibleException expected){
+        }
+    }
+
+    @Test
     public void whenBlockingNotAllowed_thenNoRetryPossibleException() {
         final LongRef ref = newLongRef();
 
-        AtomicBlock block = getGlobalStmInstance().createTransactionFactoryBuilder()
+        AtomicBlock block = getGlobalStmInstance()
+                .createTransactionFactoryBuilder()
                 .setBlockingAllowed(false)
                 .buildAtomicBlock();
 
