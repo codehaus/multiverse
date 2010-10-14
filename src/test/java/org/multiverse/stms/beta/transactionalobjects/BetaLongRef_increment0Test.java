8bae302181973eefd924946f9954f10116b9e63e
alarmnummer <alarmnummer@gmail.com>
2010-10-14 17:49:33 +0200

    - improved testing

--- src/test/java/org/multiverse/stms/beta/transactionalobjects/BetaLongRef_increment0Test.java
+++ src/test/java/org/multiverse/stms/beta/transactionalobjects/BetaLongRef_increment0Test.java
@@ -1,11 +1,169 @@
 package org.multiverse.stms.beta.transactionalobjects;
 
-/**
- * Created by IntelliJ IDEA.
- * User: alarmnummer
- * Date: Oct 13, 2010
- * Time: 11:49:00 PM
- * To change this template use File | Settings | File Templates.
- */
+import org.junit.Before;
+import org.junit.Test;
+import org.multiverse.api.exceptions.DeadTransactionException;
+import org.multiverse.api.exceptions.PreparedTransactionException;
+import org.multiverse.api.exceptions.ReadWriteConflict;
+import org.multiverse.api.exceptions.TransactionRequiredException;
+import org.multiverse.stms.beta.BetaStm;
+import org.multiverse.stms.beta.transactions.BetaTransaction;
+
+import static org.junit.Assert.fail;
+import static org.multiverse.TestUtils.assertIsAborted;
+import static org.multiverse.TestUtils.assertIsCommitted;
+import static org.multiverse.api.ThreadLocalTransaction.clearThreadLocalTransaction;
+import static org.multiverse.api.ThreadLocalTransaction.setThreadLocalTransaction;
+import static org.multiverse.stms.beta.BetaStmTestUtils.*;
+
 public class BetaLongRef_increment0Test {
+    private BetaStm stm;
+
+    @Before
+    public void setUp() {
+        stm = new BetaStm();
+        clearThreadLocalTransaction();
+    }
+
+    @Test
+    public void whenSuccess() {
+        long initialValue = 10;
+        BetaLongRef ref = newLongRef(stm, initialValue);
+        long initialVersion = ref.getVersion();
+
+        BetaTransaction tx = stm.startDefaultTransaction();
+        setThreadLocalTransaction(tx);
+
+        ref.increment();
+
+        tx.commit();
+
+        assertIsCommitted(tx);
+        assertVersionAndValue(ref, initialVersion + 1, initialValue + 1);
+    }
+
+    @Test
+    public void whenEnsuredByOther_thenIncrementSucceedsButCommitFails() {
+        long initialValue = 10;
+        BetaLongRef ref = newLongRef(stm, initialValue);
+        long initialVersion = ref.getVersion();
+
+        BetaTransaction otherTx = stm.startDefaultTransaction();
+        ref.ensure(otherTx);
+
+        BetaTransaction tx = stm.startDefaultTransaction();
+        setThreadLocalTransaction(tx);
+
+        ref.increment();
+
+        try {
+            tx.commit();
+            fail();
+        } catch (ReadWriteConflict expected) {
+        }
+
+        assertIsAborted(tx);
+        assertVersionAndValue(ref, initialVersion, initialValue);
+        assertRefHasUpdateLock(ref, otherTx);
+    }
+
+    @Test
+    public void whenPrivatizedByOther_thenIncrementSucceedsButCommitFails() {
+        long initialValue = 10;
+        BetaLongRef ref = newLongRef(stm, initialValue);
+        long initialVersion = ref.getVersion();
+
+        BetaTransaction otherTx = stm.startDefaultTransaction();
+        ref.privatize(otherTx);
+
+        BetaTransaction tx = stm.startDefaultTransaction();
+        setThreadLocalTransaction(tx);
+
+        ref.increment();
+
+        try {
+            tx.commit();
+            fail();
+        } catch (ReadWriteConflict expected) {
+        }
+
+        assertIsAborted(tx);
+        assertVersionAndValue(ref, initialVersion, initialValue);
+        assertRefHasCommitLock(ref, otherTx);
+    }
+
+    @Test
+    public void whenCommittedTransactionFound() {
+        long initialValue = 10;
+        BetaLongRef ref = newLongRef(stm, initialValue);
+        long initialVersion = ref.getVersion();
+
+        BetaTransaction tx = stm.startDefaultTransaction();
+        setThreadLocalTransaction(tx);
+        tx.commit();
+
+        try {
+            ref.increment();
+            fail();
+        } catch (DeadTransactionException expected) {
+        }
+
+        assertIsCommitted(tx);
+        assertVersionAndValue(ref, initialVersion, initialValue);
+    }
+
+    @Test
+    public void whenAbortedTransactionFound_thenDeadTransactionException() {
+        long initialValue = 10;
+        BetaLongRef ref = newLongRef(stm, initialValue);
+        long initialVersion = ref.getVersion();
+
+        BetaTransaction tx = stm.startDefaultTransaction();
+        setThreadLocalTransaction(tx);
+        tx.abort();
+
+        try {
+            ref.increment();
+            fail();
+        } catch (DeadTransactionException expected) {
+        }
+
+        assertIsAborted(tx);
+        assertVersionAndValue(ref, initialVersion, initialValue);
+    }
+
+    @Test
+    public void whenPreparedTransactionFound_thenPreparedTransactionException() {
+        long initialValue = 10;
+        BetaLongRef ref = newLongRef(stm, initialValue);
+        long initialVersion = ref.getVersion();
+
+        BetaTransaction tx = stm.startDefaultTransaction();
+        setThreadLocalTransaction(tx);
+        tx.prepare();
+
+        try {
+            ref.increment();
+            fail();
+        } catch (PreparedTransactionException expected) {
+        }
+
+        assertIsAborted(tx);
+        assertVersionAndValue(ref, initialVersion, initialValue);
+    }
+
+    @Test
+    public void whenNoTransaction_thenTransactionRequiredException() {
+        long initialValue = 10;
+        BetaLongRef ref = newLongRef(stm, initialValue);
+        long initialVersion = ref.getVersion();
+
+        try {
+            ref.increment();
+            fail();
+        } catch (TransactionRequiredException expected) {
+        }
+
+        assertVersionAndValue(ref, initialVersion, initialValue);
+    }
 }
