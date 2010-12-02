0546ee62dc478efad6e716328a50006974b7b729
alarmnummer <alarmnummer@alarmnummer-desktop.(none)>
2010-12-02 22:43:02 +0100

    some work on the orelse support

---
+++ src/main/java/org/multiverse/api/OrElseBlock.java
@@ -0,0 +1,11 @@
+package org.multiverse.api;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: alarmnummer
+ * Date: 12/2/10
+ * Time: 4:08 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class OrElseBlock {
+}
