43c139efe4066e11eb968f5b419b87019636c0ae
alarmnummer <alarmnummer@xps1530.(none)>
2010-12-26 11:46:36 +0100

    - more testing cleanup

--- multiverse-beta/src/test/java/org/multiverse/stms/beta/integrationtest/traditionalsynchronization/NonReentrantSemaphoreStressTest.java
+++ multiverse-beta/src/test/java/org/multiverse/stms/beta/integrationtest/traditionalsynchronization/NonReentrantSemaphoreStressTest.java
@@ -1,23 +1,24 @@
 package org.multiverse.stms.beta.integrationtest.traditionalsynchronization;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.multiverse.TestThread;
 import org.multiverse.TestUtils;
 import org.multiverse.api.AtomicBlock;
+import org.multiverse.api.LockLevel;
 import org.multiverse.api.Transaction;
 import org.multiverse.api.closures.AtomicVoidClosure;
+import org.multiverse.api.references.IntRef;
 import org.multiverse.stms.beta.BetaStm;
-import org.multiverse.stms.beta.transactionalobjects.BetaIntRef;
-import org.multiverse.stms.beta.transactionalobjects.BetaIntRefTranlocal;
-import org.multiverse.stms.beta.transactions.BetaTransaction;
 
+import java.util.concurrent.atomic.AtomicLong;
+
+import static org.junit.Assert.fail;
 import static org.multiverse.TestUtils.*;
 import static org.multiverse.api.GlobalStmInstance.getGlobalStmInstance;
+import static org.multiverse.api.StmUtils.newIntRef;
 import static org.multiverse.api.StmUtils.retry;
 import static org.multiverse.api.ThreadLocalTransaction.clearThreadLocalTransaction;
-import static org.multiverse.stms.beta.BetaStmTestUtils.newIntRef;
 
 /**
  * A StressTest that checks if a the Semaphore; a traditional synchronization structure can be build
@@ -28,6 +29,8 @@ public class NonReentrantSemaphoreStressTest {
     private boolean pessimistic;
     private volatile boolean stop;
     private int threadCount = 10;
+    private int resourceCount = 5;
+    private Semaphore semaphore;
 
     @Before
     public void setUp() {
@@ -37,85 +40,109 @@ public class NonReentrantSemaphoreStressTest {
     }
 
     @Test
-    @Ignore
-    public void testPessimistic() {
-        test(true);
+    public void whenNoLocking() {
+        test(LockLevel.LockNone);
     }
 
     @Test
-    @Ignore
-    public void testOptimistic() {
-        test(false);
+    public void whenWriteLockReads() {
+        test(LockLevel.WriteLockReads);
     }
 
-    public void test(boolean pessimistic) {
-        this.pessimistic = pessimistic;
+    @Test
+    public void whenWriteLockWrites() {
+        test(LockLevel.WriteLockWrites);
+    }
 
-        WorkerThread[] threads = new WorkerThread[threadCount];
-        for (int k = 0; k < threads.length; k++) {
-            threads[k] = new WorkerThread(k);
+    @Test
+    public void whenCommitLockReads() {
+        test(LockLevel.CommitLockReads);
+    }
+
+    @Test
+    public void whenCommitLockWrites() {
+        test(LockLevel.CommitLockWrites);
+    }
+
+    public void test(LockLevel lockLevel) {
+        semaphore = new Semaphore(resourceCount, lockLevel);
+
+        WorkerThread[] workers = new WorkerThread[threadCount];
+        for (int k = 0; k < threadCount; k++) {
+            workers[k] = new WorkerThread(k);
         }
 
-        startAll(threads);
-        sleepMs(TestUtils.getStressTestDurationMs(60 * 1000));
+        startAll(workers);
+        sleepMs(TestUtils.getStressTestDurationMs(30 * 1000));
+        System.out.println("Terminating");
         stop = true;
-        joinAll(threads);
-
-        //assertEquals(sum(threads), sum(intValues));
-        //System.out.println("total increments: " + sum(threads));
+        System.out.println(semaphore.ref.toDebugString());
+        joinAll(workers);
     }
 
     class WorkerThread extends TestThread {
+        long count;
+
         public WorkerThread(int id) {
-            super("Worker-" + id);
+            super("Producer-" + id);
         }
 
         @Override
         public void doRun() throws Exception {
             while (!stop) {
+                semaphore.down();
+                semaphore.up();
+                count++;
 
+                if (count % 1000000 == 0) {
+                    System.out.printf("%s is at %s\n", getName(), count);
+                }
             }
         }
     }
 
     class Semaphore {
 
-        private final BetaIntRef ref;
-        private final AtomicBlock upBlock = stm.createTransactionFactoryBuilder().buildAtomicBlock();
-        private final AtomicBlock downBlock = stm.createTransactionFactoryBuilder().buildAtomicBlock();
-        private final AtomicVoidClosure upClosure = new AtomicVoidClosure() {
-            @Override
-            public void execute(Transaction tx) throws Exception {
-                BetaTransaction btx = (BetaTransaction) tx;
+        private IntRef ref;
+        private AtomicLong users = new AtomicLong();
+        private AtomicBlock upBlock;
+        private AtomicBlock downBlock;
 
-                BetaIntRefTranlocal write = btx.openForWrite(ref, pessimistic ? LOCKMODE_COMMIT : LOCKMODE_NONE);
-                write.value++;
-            }
-        };
-        private final AtomicVoidClosure downClosure = new AtomicVoidClosure() {
-            @Override
-            public void execute(Transaction tx) throws Exception {
-                BetaTransaction btx = (BetaTransaction) tx;
-
-                BetaIntRefTranlocal write = btx.openForWrite(ref, pessimistic ? LOCKMODE_COMMIT : LOCKMODE_NONE);
-                if (write.value == 0) {
-                    retry();
-                }
-
-                write.value--;
-            }
-        };
-
-        public Semaphore(int initial) {
-            ref = newIntRef(stm, initial);
+        public Semaphore(int initial, LockLevel lockLevel) {
+            ref = newIntRef(initial);
+            upBlock = stm.createTransactionFactoryBuilder()
+                    .setLockLevel(lockLevel).buildAtomicBlock();
+            downBlock = stm.createTransactionFactoryBuilder()
+                    .setLockLevel(lockLevel).buildAtomicBlock();
         }
 
         public void up() {
-            upBlock.execute(upClosure);
+            upBlock.execute(new AtomicVoidClosure() {
+                @Override
+                public void execute(Transaction tx) throws Exception {
+                    ref.increment();
+                }
+            });
+
+            users.incrementAndGet();
+            if (users.get() > resourceCount) {
+                fail();
+            }
         }
 
         public void down() {
-            downBlock.execute(downClosure);
+            users.decrementAndGet();
+
+            downBlock.execute(new AtomicVoidClosure() {
+                @Override
+                public void execute(Transaction tx) throws Exception {
+                    if (ref.get() == 0) {
+                        retry();
+                    }
+
+                    ref.decrement();
+                }
+            });
         }
     }
 }
--- multiverse-beta/src/test/java/org/multiverse/stms/beta/integrationtest/traditionalsynchronization/ReentrantMutexStressTest.java
+++ multiverse-beta/src/test/java/org/multiverse/stms/beta/integrationtest/traditionalsynchronization/ReentrantMutexStressTest.java
@@ -1,24 +1,118 @@
 package org.multiverse.stms.beta.integrationtest.traditionalsynchronization;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.multiverse.TestThread;
+import org.multiverse.api.Transaction;
+import org.multiverse.api.closures.AtomicVoidClosure;
+import org.multiverse.api.references.IntRef;
+import org.multiverse.api.references.Ref;
 import org.multiverse.stms.beta.BetaStm;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.multiverse.TestUtils.*;
 import static org.multiverse.api.GlobalStmInstance.getGlobalStmInstance;
+import static org.multiverse.api.StmUtils.*;
 import static org.multiverse.api.ThreadLocalTransaction.clearThreadLocalTransaction;
 
 public class ReentrantMutexStressTest {
     private BetaStm stm;
+    private int threadCount = 10;
+    private ReentrantMutex mutex;
+    private volatile boolean stop;
 
     @Before
     public void setUp() {
         stm = (BetaStm) getGlobalStmInstance();
         clearThreadLocalTransaction();
+        stop = false;
     }
 
     @Test
-    @Ignore
     public void test() {
+        mutex = new ReentrantMutex();
+        StressThread[] threads = new StressThread[threadCount];
+        for (int k = 0; k < threads.length; k++) {
+            threads[k] = new StressThread(k);
+        }
+
+        startAll(threads);
+        sleepMs(30000);
+        stop = true;
+        joinAll(threads);
+    }
+
+    class StressThread extends TestThread {
+        public StressThread(int id) {
+            super("StressThread-" + id);
+        }
+
+        @Override
+        public void doRun() throws Exception {
+            long count = 0;
+            while (!stop) {
+                mutex.lock(this);
+
+                boolean nested = randomOneOf(3);
+                if(nested){
+                    mutex.lock(this);
+                }
+
+                sleepRandomMs(100);
+                mutex.unlock(this);
+
+                if(nested){
+                    mutex.unlock(this);
+                }
+
+                count++;
+                if (count % 10 == 0) {
+                    System.out.printf("%s is at %s\n", getName(), count);
+                }
+            }
+        }
+    }
+
+    class ReentrantMutex {
+        private final Ref<Thread> owner = newRef();
+        private final IntRef count = newIntRef();
+
+        public void lock(final Thread thread) {
+            execute(new AtomicVoidClosure() {
+                @Override
+                public void execute(Transaction tx) throws Exception {
+                    if (owner.get() == null) {
+                        owner.set(thread);
+                        count.increment();
+                        return;
+                    }
+
+                    if (owner.get() == thread) {
+                        count.increment();
+                        return;
+                    }
+
+                    retry();
+                }
+            });
+        }
+
+        public void unlock(final Thread thread) {
+            execute(new AtomicVoidClosure() {
+                @Override
+                public void execute(Transaction tx) throws Exception {
+                    if (owner.get() != thread) {
+                        throw new IllegalMonitorStateException();
+                    }
+
+                    count.decrement();
+                    if (count.get() == 0) {
+                        owner.set(null);
+                    }
+                }
+            });
+        }
     }
 }
