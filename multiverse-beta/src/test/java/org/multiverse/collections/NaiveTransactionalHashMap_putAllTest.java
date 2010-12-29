af1a812c901af8dfd4c5abbe495672ec815c3ea9
alarmnummer <alarmnummer@alarmnummer-desktop.(none)>
2010-12-29 20:50:53 +0100

    - more collections cleanup

--- multiverse-beta/src/test/java/org/multiverse/collections/NaiveTransactionalHashMap_putAllTest.java
+++ multiverse-beta/src/test/java/org/multiverse/collections/NaiveTransactionalHashMap_putAllTest.java
@@ -7,6 +7,7 @@ import org.multiverse.api.Transaction;
 import org.multiverse.api.closures.AtomicVoidClosure;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
@@ -43,30 +44,71 @@ public class NaiveTransactionalHashMap_putAllTest {
     }
 
     @Test
-    public void whenEmptyMapAdded(){
+    public void whenEmptyMapAdded() {
+        execute(new AtomicVoidClosure() {
+            @Override
+            public void execute(Transaction tx) throws Exception {
+                map.putAll(new HashMap<String, String>());
 
-         execute(new AtomicVoidClosure() {
-             @Override
-             public void execute(Transaction tx) throws Exception {
-                 map.putAll(new HashMap<String,String>());
-
-                 assertEquals(0, map.size());
-             }
-         });
+                assertEquals(0, map.size());
+            }
+        });
     }
 
     @Test
-    public void whenOneOfTheItemsIsNull(){
-
-    }
-
-    @Test
-    public void whenAllDifferentItems(){
+    public void whenOneOfTheItemsIsNull() {
 
     }
 
     @Test
-    public void whenSomeItemsReplaced(){
+    public void whenAllDifferentItems() {
+        execute(new AtomicVoidClosure() {
+            @Override
+            public void execute(Transaction tx) throws Exception {
+                map.put("1", "a");
+                map.put("2", "b");
+                map.put("3", "c");
 
+                Map<String, String> other = new HashMap<String, String>();
+                other.put("4", "d");
+                other.put("5", "e");
+                other.put("6", "f");
+
+                map.putAll(other);
+
+                assertEquals(6, map.size());
+                assertEquals("a", map.get("1"));
+                assertEquals("b", map.get("2"));
+                assertEquals("c", map.get("3"));
+                assertEquals("d", map.get("4"));
+                assertEquals("e", map.get("5"));
+                assertEquals("f", map.get("6"));
+            }
+        });
+    }
+
+    @Test
+    public void whenSomeItemsReplaced() {
+        execute(new AtomicVoidClosure() {
+            @Override
+            public void execute(Transaction tx) throws Exception {
+                map.put("1", "a");
+                map.put("2", "b");
+                map.put("3", "c");
+
+                Map<String, String> other = new HashMap<String, String>();
+                other.put("4", "d");
+                other.put("2", "B");
+                other.put("3", "C");
+
+                map.putAll(other);
+
+                assertEquals(4, map.size());
+                assertEquals("a", map.get("1"));
+                assertEquals("B", map.get("2"));
+                assertEquals("C", map.get("3"));
+                assertEquals("d", map.get("4"));
+            }
+        });
     }
 }
