0158f82ab01b98a704f5dc5bb18dd3bf0cb7bf61
alarmnummer <alarmnummer@alarmnummer-desktop.(none)>
2011-01-27 22:48:06 +0100

    - testing for lean tx and register

--- multiverse-beta/src/test/java/org/multiverse/stms/gamma/transactions/lean/LeanGammaTransaction_registerTest.java
+++ multiverse-beta/src/test/java/org/multiverse/stms/gamma/transactions/lean/LeanGammaTransaction_registerTest.java
@@ -2,8 +2,18 @@ package org.multiverse.stms.gamma.transactions.lean;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.multiverse.api.exceptions.DeadTransactionException;
+import org.multiverse.api.exceptions.PreparedTransactionException;
+import org.multiverse.api.exceptions.SpeculativeConfigurationError;
+import org.multiverse.api.lifecycle.TransactionListener;
 import org.multiverse.stms.gamma.GammaStm;
 import org.multiverse.stms.gamma.transactions.GammaTransaction;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.multiverse.TestUtils.assertIsAborted;
+import static org.multiverse.TestUtils.assertIsCommitted;
 
 public abstract class LeanGammaTransaction_registerTest<T extends GammaTransaction> {
 
@@ -18,21 +28,84 @@ public abstract class LeanGammaTransaction_registerTest<T extends GammaTransacti
 
     @Test
     public void whenNullListener_thenNullPointerException() {
+        T tx = newTransaction();
+        try {
+            tx.register(null);
+            fail();
+        } catch (NullPointerException expected) {
 
+        }
+
+        assertIsAborted(tx);
+        assertFalse(tx.getConfiguration().getSpeculativeConfiguration().areListenersDetected);
     }
 
     @Test
-    public void whenSuccess_thenSpeculativeConfigurationError() {
+    public void whenSuccess() {
+        T tx = newTransaction();
+        TransactionListener listener = mock(TransactionListener.class);
 
+        try {
+            tx.register(listener);
+            fail();
+        } catch (SpeculativeConfigurationError expected) {
+
+        }
+
+        assertIsAborted(tx);
+        assertTrue(tx.getConfiguration().getSpeculativeConfiguration().areListenersDetected);
+        assertNull(tx.listeners);
+        verifyZeroInteractions(listener);
     }
 
     @Test
-    public void whenCommitted() {
+    public void whenAlreadyPrepared() {
+        T tx = newTransaction();
+        tx.prepare();
 
+        TransactionListener listener = mock(TransactionListener.class);
+        try {
+            tx.register(listener);
+            fail();
+        } catch (PreparedTransactionException expected) {
+        }
+
+        assertIsAborted(tx);
+        verifyZeroInteractions(listener);
+        assertFalse(tx.getConfiguration().getSpeculativeConfiguration().areListenersDetected);
     }
 
     @Test
-    public void whenAborted() {
+    public void whenAlreadyAborted() {
+        T tx = newTransaction();
+        tx.abort();
 
+        TransactionListener listener = mock(TransactionListener.class);
+        try {
+            tx.register(listener);
+            fail();
+        } catch (DeadTransactionException expected) {
+        }
+
+        assertIsAborted(tx);
+        verifyZeroInteractions(listener);
+        assertFalse(tx.getConfiguration().getSpeculativeConfiguration().areListenersDetected);
+    }
+
+    @Test
+    public void whenAlreadyCommitted() {
+        T tx = newTransaction();
+        tx.commit();
+
+        TransactionListener listener = mock(TransactionListener.class);
+        try {
+            tx.register(listener);
+            fail();
+        } catch (DeadTransactionException expected) {
+        }
+
+        assertIsCommitted(tx);
+        verifyZeroInteractions(listener);
+        assertFalse(tx.getConfiguration().getSpeculativeConfiguration().areListenersDetected);
     }
 }
