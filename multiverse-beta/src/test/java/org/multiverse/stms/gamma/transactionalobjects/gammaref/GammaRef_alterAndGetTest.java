3b8c1d7ed1037a63244c2cb736529415e367682a
alarmnummer <alarmnummer@alarmnummer-desktop.(none)>
2011-02-08 23:14:02 +0100

    - added empty test bodies for missing tests

---
+++ multiverse-beta/src/test/java/org/multiverse/stms/gamma/transactionalobjects/gammaref/GammaRef_alterAndGetTest.java
@@ -0,0 +1,11 @@
+package org.multiverse.stms.gamma.transactionalobjects.gammaref;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class GammaRef_alterAndGetTest {
+
+    @Test
+    @Ignore
+    public void test(){}
+}
---
+++ multiverse-beta/src/test/java/org/multiverse/stms/gamma/transactionalobjects/gammaref/GammaRef_atomicAlterAndGetTest.java
@@ -0,0 +1,10 @@
+package org.multiverse.stms.gamma.transactionalobjects.gammaref;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class GammaRef_atomicAlterAndGetTest {
+    @Test
+    @Ignore
+    public void test(){}
+}
