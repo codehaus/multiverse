554b4f0c54dfc69a25ce8d7712de9625d978d7f8
alarmnummer <alarmnummer@alarmnummer-desktop.(none)>
2011-02-17 00:25:16 +0100

    - more improved javadoc

--- multiverse-gamma/src/main/java/org/multiverse/api/Stm.java
+++ multiverse-gamma/src/main/java/org/multiverse/api/Stm.java
@@ -23,22 +23,23 @@ import org.multiverse.api.references.RefFactoryBuilder;
  *
  * <p>The Stm interface provides a mechanism to separate the contract from the implementation. So it is possible to change the
  * Stm implementation without changing the code that uses it. The idea is that for example a TL2 (MVCC) based implementation can
- * be replaced by a SkySTM or a lock based STM. Of course every Stm implementation will have its strong and weak
+ * be replaced by a Sky-STM or a lock based STM. Of course every Stm implementation will have its strong and weak
  * spots.
  *
  * <p>All functionality like AtomicBlocks, Refs, Transaction etc can be customized by providing a custom implementation of the
  * factory/builder interfaces:
  * <ol>
- *     <li>{@link RefFactoryBuilder} a builder for creating {@link RefFactory}</li>
- *     <li>{@link TransactionFactoryBuilder} a builder for creating an {@link AtomicBlock}/{@link Transaction}.
- *     <li>{@link TransactionalCollectionsFactory} a factory for creating transactional collections</li>
+ * <li>{@link RefFactoryBuilder} a builder for creating {@link RefFactory}</li>
+ * <li>{@link TransactionFactoryBuilder} a builder for creating an {@link AtomicBlock}/{@link Transaction}.
+ * <li>{@link TransactionalCollectionsFactory} a factory for creating transactional collections</li>
  * </ol>
  *
  * <h3>Multiple Stm instances</h3>
  *
- * <p>It is important that an TransactionalObject only is used within a single stm. If it is 'shared' between different
+ * <p>It is important that an TransactionalObject only is used within a single Stm. If it is 'shared' between different
  * stm instances, isolation problems could happen. This can be caused by the fact that different stm instances
- * probably use different clocks or completely different mechanisms for preventing isolation problems.
+ * probably use different clocks or completely different mechanisms for preventing isolation problems. It depends on the
+ * implementation if any checking is done (the GammaStm does check if there is a conflict).
  *
  * <h3>Thread safe</h3>
  * All methods on the Stm are of course thread safe.
@@ -48,30 +49,30 @@ import org.multiverse.api.references.RefFactoryBuilder;
 public interface Stm {
 
     /**
-     * Gets the {@link TransactionFactoryBuilder} that needs to be used to execute transactions on this Stm. See the
-     * {@link TransactionFactoryBuilder} for more info.
+     * Gets the {@link TransactionFactoryBuilder} that needs to be used to execute a {@link Transaction} created by this Stm.
+     * See the {@link TransactionFactoryBuilder} for more info. The TransactionFactoryBuilder also is responsible for creating
+     * the AtomicBlock since the Transaction and AtomicBlock can be tightly coupled.
      *
-     * @return the TransactionFactoryBuilder that needs to be used to execute transactions on this Stm.
+     * @return the TransactionFactoryBuilder that is used to execute transactions on this Stm.
      */
     TransactionFactoryBuilder newTransactionFactoryBuilder();
 
     /**
-     * Starts a default Transaction that is useful for testing/experimentation purposes. This method is purely
-     * for easy to use access, but doesn't provide any configuration options. See the
-     * {@link #newTransactionFactoryBuilder()} for something more configurable.
-     * <p/>
-     * Transactions returned by this method are not speculative.
+     * Starts a default Transaction that is useful for testing/experimentation purposes. This method is purely for easy to use access,
+     * but doesn't provide any configuration options. See the {@link #newTransactionFactoryBuilder()} for something more configurable.
+     * In mose cases this is not the method you want to use to manage transactions.
+     *
+     * <p>Transactions returned by this method are not speculative.
      *
      * @return the new default Transaction.
      */
     Transaction newDefaultTransaction();
 
     /**
-     * Returns the default atomic block that is useful for testing/experimentation purposes. This method is purely
-     * for easy to use access, but it doesn't provide any configuration options. See the
-     * {@link #newTransactionFactoryBuilder()} for something more configurable.
-     * <p/>
-     * Transactions used in this Block are not speculative.
+     * Returns the default atomic block that is useful for testing/experimentation purposes. This method is purely for easy to use access,
+     * but it doesn't provide any configuration options. See the {@link #newTransactionFactoryBuilder()} for something more configurable.
+     *
+     * <p>Transactions used in this Block are not speculative.
      *
      * @return the default AtomicBlock.
      */
