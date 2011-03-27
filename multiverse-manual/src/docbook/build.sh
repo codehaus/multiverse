62be41d7f737d3ee0185f008fd78091adcd8e08c
alarmnummer <alarmnummer@alarmnummer-desktop.(none)>
2011-03-27 16:54:09 +0200

    - added docbook

---
+++ multiverse-manual/src/docbook/build.sh
@@ -0,0 +1,8 @@
+xsltproc \
+ --stringparam chunk.section.depth 0 \
+ --stringparam section.autolabel 1 \
+ --stringparam section.label.includes.component.label 1 \
+ --stringparam html.stylesheet style.css \
+ -o ../../build/manual.html /usr/share/xml/docbook/stylesheet/nwalsh/xhtml/chunk.xsl manual.xml
+
+cp style.css ../../build/
