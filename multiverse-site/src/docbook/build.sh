2b042c3bdd627a5ce215ec418f3dcb464abe1231
alarmnummer <alarmnummer@alarmnummer-desktop.(none)>
2011-03-31 21:49:30 +0200

    - more manual improvements

--- multiverse-site/src/docbook/build.sh
+++
@@ -1,8 +0,0 @@
-xsltproc \
- --stringparam chunk.section.depth 0 \
- --stringparam section.autolabel 1 \
- --stringparam section.label.includes.component.label 1 \
- --stringparam html.stylesheet style.css \
- -o ../../build/site/manual/manual.html /usr/share/xml/docbook/stylesheet/nwalsh/xhtml/chunk.xsl manual.xml
-
-cp style.css ../../build/site/manual
